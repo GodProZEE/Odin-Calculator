014abd89c42442e47589d01795cbbf0ed425f414
GodProZEE <100044640+GodProZEE@users.noreply.github.com>
2024-07-22 22:56:59 +0500

    Add negative numbers, correct display and funcs
    Negative number support has been added by putting the operators inside arrays, display has been corrected by using different values for displays based on numbers, all functions have been set to work correctly with all kinds of numbers.

--- script.js
+++ script.js
@@ -19,26 +19,24 @@ function divide(a, b) {
 }
 
 
-let firstNumber;
-let operator;
-let secondNumber;
+
 
 function operate(num1, operator, num2) {
     switch (operator) {
         case '+':
-            add(num1, num2);
+            return(add(num1, num2));
             break;
 
         case '-':
-            subtract(num1, num2);
+            return(subtract(num1, num2));
             break;
         
         case '*':
-            multiply(num1, num2);
+            return(multiply(num1, num2));
             break;
         
         case '/':
-            divide(num1, num2);
+            return(divide(num1, num2));
             break;
 
     
@@ -48,4 +46,83 @@ function operate(num1, operator, num2) {
 }
 
 const displayArea = document.querySelector('.displayArea')
-displayArea.textContent = 'hewwo'
+let result;
+let displayValue = '';
+let secondDisplay = ''
+const container = document.querySelector('.container')
+
+let firstNumber = '';
+let operator = '';
+let secondNumber;
+let actualFirstNumber = null;
+let actualSecondNumber = null;
+let secondFlag = false
+console.log(actualFirstNumber === null)
+let secondOperator;
+let tempOp;
+let arrayOfOperator;
+let firstOperator;
+
+container.addEventListener('click', (e) => {
+    if (e.target.classList.contains('operation')) {
+        operator += e.target.textContent;
+    } else {
+    if (e.target.classList.contains('number') && operator === '') {
+        displayValue += e.target.textContent;
+        displayArea.textContent = displayValue;
+        firstNumber = displayValue;
+        actualFirstNumber = Number(firstNumber)
+
+    } else {
+        if (operator !== '' && !(e.target.classList.contains('equals'))) {
+ 
+                secondDisplay += e.target.textContent;
+                displayArea.textContent = secondDisplay;
+                secondNumber = secondDisplay;
+                actualSecondNumber = Number(secondNumber)
+
+        } else {
+            if (e.target.classList.contains('equals') && (typeof actualSecondNumber === 'number' && operator !== '=')) {
+                console.log('uhm');
+                arrayOfOperator = operator.split('')
+                firstOperator = arrayOfOperator[0]
+                if (arrayOfOperator.length >= 2) {
+                    secondOperator = arrayOfOperator[1, arrayOfOperator.length - 1]
+                    actualSecondNumber = Number(secondOperator + actualSecondNumber)
+                }
+                console.log(arrayOfOperator)
+                console.log(firstOperator)
+                console.log(secondOperator)
+                console.log(operator)
+                if (actualFirstNumber === null) {
+                    actualFirstNumber = 0;
+                    result = operate(actualFirstNumber, firstOperator, actualSecondNumber);
+                    actualFirstNumber = Number(result);
+                    displayArea.textContent = Number(result);
+                    operator = ''
+                    secondDisplay = ''
+                    actualSecondNumber = null;
+                    arrayOfOperator = []
+                    firstOperator = ''
+                    secondOperator = ''
+                } else {
+                    result = operate(actualFirstNumber, firstOperator, actualSecondNumber);
+                    actualFirstNumber = Number(result);
+                    displayArea.textContent = Number(result);
+                    operator = ''
+                    secondDisplay = ''
+                    actualSecondNumber = null;
+                    displayValue = ''
+                    arrayOfOperator = []
+                    firstOperator = ''
+                    secondOperator = ''
+                }
+                
+            }
+
+        }
+    }
+    }
+})
+
+
