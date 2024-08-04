09cad850aa98c7ef84a7e7673ac909c19fe92d7c
GodProZEE <100044640+GodProZEE@users.noreply.github.com>
2024-08-04 19:37:03 +0500

    Add rounding, negative number, decimal, bold font
    Plus-Minus button was removed and the entire javascript code was copied from another file but not much changes were made, AC function, don't click button, decimal functionality were added.

--- script.js
+++ script.js
@@ -1,150 +1,189 @@
 function add(a, b) {
-    // console.log(a+b);
     return a+b;
 }
 
 function subtract(a, b) {
-    // console.log(a-b);
     return a-b;
 }
 
 function multiply(a, b) {
-    // console.log(a*b);
     return a*b;
 }
 
 function divide(a, b) {
-    // console.log(a/b);
     return a/b;
 }
 
 
-
-
-function operate(num1, operator, num2) {
+function operate(firstNumber, operator, secondNumber) {
     switch (operator) {
         case '+':
-            return(add(num1, num2));
-            break;
+            return add(firstNumber, secondNumber)
 
         case '-':
-            return(subtract(num1, num2));
-            break;
-        
-        case '*':
-            return(multiply(num1, num2));
-            break;
-        
-        case '/':
-            return(divide(num1, num2));
-            break;
+            return subtract(firstNumber, secondNumber)  
 
+        case '*':
+            return multiply(firstNumber, secondNumber)
+            
+        case '/':
+            return divide(firstNumber, secondNumber)
     
         default:
             break;
     }
 }
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let operatorArray = ["+", "-", "/", "*"];
 
-const displayArea = document.querySelector('.displayArea')
-let result;
-let displayValue = '';
-let secondDisplay = ''
-const container = document.querySelector('.container')
+const calculator = document.querySelector('.entireCalculator');
+const container = document.querySelector('.container');
+let displayArea = document.querySelector('.displayArea')
 
-let firstNumber = '';
-let operator = '';
-let secondNumber;
+let statement = ''
+let firstNumber = ''
+let secondNumber = ''
+let firstOperator = ''  // This is for the sign of the first number (positive by default)
+let secondOperator = '' // This is for the actual operation being performed
+let thirdOperator = ''  // This is for the sign of the second number (positive by default)
+let result = ''
 let actualFirstNumber = null;
 let actualSecondNumber = null;
-let secondFlag = false
-// console.log(actualFirstNumber === null)
-let secondOperator;
-let tempOp;
-let arrayOfOperator;
-let firstOperator;
-let operationDone = false;
+let displayValue = ''
+let secondDisplay = ''
+let decimal  = '.'
+function searchForDecimal(string) {
+    let flag = false;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === decimal) {
+            flag = true;
+        }
+    }
+    return flag;
+}
 function operateAndReduce() {
-    result = operate(actualFirstNumber, firstOperator, actualSecondNumber);
+    result = Math.round(((operate(actualFirstNumber, secondOperator, actualSecondNumber)) + Number.EPSILON) * 10000) / 10000
+    console.log(statement.at(-1))
+    console.log(result)
+    if (isNaN(result)) {
+        displayArea.setAttribute('style', 'font-size: 18px')
+        result = "Bruh what are you trying to do"
+    } else {
+        if (result === Infinity) {
+            displayArea.setAttribute('style', 'font-size: 18px')
+            result = "Seriously wth are you trying to do"
+        }
+    }
+
+    if (operatorArray.includes(statement.at(-1))) {
+        secondOperator = statement.at(-1);
+    } else {
+        secondOperator = ''
+    }
     actualFirstNumber = Number(result);
-    displayArea.textContent = Number(result);
-    operator = ''
+    displayArea.textContent = result;
+    actualSecondNumber = null;
+    displayValue = actualFirstNumber;
     secondDisplay = ''
+    secondNumber = ''
+    statement = String(result)
+    thirdOperator = ''
+
+}
+function clearEverything() {
+    statement = ''
+    firstNumber = ''
+    secondNumber = ''
+    firstOperator = ''  // This is for the sign of the first number (positive by default)
+    secondOperator = '' // This is for the actual operation being performed
+    thirdOperator = ''  // This is for the sign of the second number (positive by default)
+    result = ''
+    actualFirstNumber = null;
     actualSecondNumber = null;
     displayValue = ''
-    arrayOfOperator = []
-    firstOperator = ''
-    secondOperator = ''
-    operationDone = false;
+    secondDisplay = ''
+    decimal  = '.'
+    displayArea.textContent = ''
 }
-    
+
 container.addEventListener('click', (e) => {
-    if (e.target.classList.contains('operation')) {
-        operator += e.target.textContent;
+    if (e.target.classList.contains('clickNo')) {
+        displayArea.setAttribute('style', 'font-size: 18px')
+        displayArea.textContent = 'What do you get by doing stuff like this';
+        setTimeout(clearEverything, 3000)
     } else {
-    if (e.target.classList.contains('number') && operator === '') {
-        displayValue += e.target.textContent;
-        displayArea.textContent = displayValue;
-        firstNumber = displayValue;
-        actualFirstNumber = Number(firstNumber)
+        displayArea.setAttribute('style', 'font-size: 40px')
+    }
 
+    if (e.target.classList.contains('clear')) {
+        clearEverything()
     } else {
-        if (operator.length <= 2 && !(e.target.classList.contains('equals')) && operationDone == false) {
- 
-                secondDisplay += e.target.textContent;
-                displayArea.textContent = secondDisplay;
-                secondNumber = secondDisplay;
-                actualSecondNumber = Number(secondNumber)
-
+    if (e.target.classList.contains('number') || e.target.classList.contains('operation') || e.target.classList.contains('equals') || e.target.classList.contains('decimal')) {
+        statement += e.target.textContent;
+        if (operatorArray.includes(statement[0]) && firstOperator == '') {
+            firstOperator = statement[0];
+            statement = statement.slice(1)
+            displayValue += statement;
+            displayArea.textContent = displayValue;
+            
         } else {
-            if (e.target.classList.contains('equals') && (typeof actualSecondNumber === 'number' && operator !== '=')) {
-                // console.log('uhm');
-                console.log(operator);
-                arrayOfOperator = operator.split('')
-                firstOperator = arrayOfOperator[0]
-                console.log(firstOperator)
-                if (arrayOfOperator.length >= 2) {
-                    secondOperator = arrayOfOperator[1, arrayOfOperator.length - 1]
-                    // console.log(secondOperator)
-                    actualSecondNumber = Number(secondOperator + actualSecondNumber)
-                    console.log(actualSecondNumber)
-                }
-                // console.log(arrayOfOperator)
-                // console.log(firstOperator)
-                // console.log(operator)
-                if (actualFirstNumber === null) {
-                    actualFirstNumber = 0;
-                    operateAndReduce()
-                } else {
-                    operateAndReduce()
-                }
-                
-            } else {
-                
-                if (typeof actualSecondNumber === 'number' && operator.length > 1) {
-                    let lastOperator = operator[operator.length - 1]
-                    console.log(lastOperator);
-                    operator = operator.slice(0, operator.length - 1)
-                    arrayOfOperator = operator.split('')
-                    firstOperator = arrayOfOperator[0]
-                    if (arrayOfOperator.length >= 2) {
-                        secondOperator = arrayOfOperator[1, arrayOfOperator.length - 1]
-                        actualSecondNumber = Number(secondOperator + actualSecondNumber)
-                    }
-                    operateAndReduce()
-                    operationDone = true;
-                    operator = lastOperator;
+            if (firstOperator === '') {
+                firstOperator = "+";
+            }
+        }
 
-                    secondDisplay += e.target.textContent
-                    displayArea.textContent = secondDisplay;
-                    secondNumber = secondDisplay;
-                    actualSecondNumber = Number(secondNumber);
+        console.log(searchForDecimal(firstNumber))
+        if ((e.target.classList.contains('number') || e.target.classList.contains('decimal')) && secondOperator === '' && thirdOperator === '') {
+                if (searchForDecimal(firstNumber) === true && e.target.classList.contains('decimal')) {
+                } else {
+                displayValue += e.target.textContent;
+                firstNumber = firstOperator + displayValue;
+                if (firstNumber[0] === '+') {
+                displayArea.textContent = firstNumber.slice(1);
+                } else {
+                    displayArea.textContent = firstNumber;
                 }
             }
+            
+            
+
+            actualFirstNumber = Number(firstNumber)
+        } else {
+            if (e.target.classList.contains('operation') && actualFirstNumber !== null && secondOperator == '' && thirdOperator === '') {
+                secondOperator = e.target.textContent;                
+            } else {
+                if (e.target.classList.contains('operation') && secondOperator !== '' && thirdOperator == '') {
+                    thirdOperator = e.target.textContent;
+                    console.log("thirdOperator being set automatically")
+                    
+                }
+        }
+
+            if ((e.target.classList.contains('number') || e.target.classList.contains('decimal')) && secondOperator !== '') {
+                if (thirdOperator === '') {
+                    thirdOperator = '+'
+                }
+                if (searchForDecimal(secondNumber) === true && e.target.classList.contains('decimal')) {
+                } else {
+                secondDisplay += e.target.textContent;
+                secondNumber = thirdOperator + secondDisplay
+                if (secondNumber[0] === '+') {
+                    displayArea.textContent = secondNumber.slice(1);
+                } else {
+                    displayArea.textContent = secondNumber;
+                }
+            }
+                
+
+                actualSecondNumber = Number(secondNumber);
+            }
+
+                if (actualFirstNumber !== null && actualSecondNumber !== null && secondOperator !== '' && thirdOperator !== '' && ((e.target.classList.contains('operation')) || e.target.classList.contains('equals'))) {
+                    operateAndReduce()
+                }
+                                
 
         }
     }
-    
 }
-})
-
-
+}) 
